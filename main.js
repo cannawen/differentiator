2a8579a0307ca0eeac568ddf7115fb967ad2b804
Canna Wen <cannawen@gmail.com>
2026-01-16 14:36:14 -0500

    Separating the calculation from the stringifying

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const assert = require('assert');
 
-derivative("2x")
+console.log(derivativeToString(derivative("2")))
+console.log(derivativeToString(derivative("2x")))
 
 // assume variable is always named x
 function derivative(equation) {
     const matches = [... equation.matchAll(/(\d+)x/g)];
     return matches.map((match) => {
-        console.log(match[1])
         return match[1];
-    })[0]
+    })
+}
+
+function derivativeToString(derivatives) {
+    return derivatives.reduce((memo, derivative) => {
+        return memo += derivative;
+    }, "")
 }
 
 // pass in any command line arg to enable tests
