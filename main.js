779add2482b8881046dc2cc463913b37c0a6efc9
Canna Wen <cannawen@gmail.com>
2026-01-16 14:31:30 -0500

    Alright giving up, using the spread operator. First test failing.

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ derivative("2x")
 
 // assume variable is always named x
 function derivative(equation) {
-    const matches = equation.matchAll(/(\d+)x/g);
-    console.log(matches)
-    matches.forEach((e) => {
-        console.log(e)
-    })
+    const matches = [... equation.matchAll(/(\d+)x/g)];
+    return matches.map((match) => {
+        console.log(match[1])
+        return match[1];
+    })[0]
 }
 
 // pass in any command line arg to enable tests
 if (process.argv[2]) {
-    assert.strictEqual(0,derivative("2"));
-    assert.strictEqual(2,derivative("2x"));
+    assert.strictEqual("0",derivative("2"));
+    assert.strictEqual("2",derivative("2x"));
 }
 
