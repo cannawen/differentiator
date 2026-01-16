eaa57733a5ce174881784e022c90517225df442e
Canna Wen <cannawen@gmail.com>
2026-01-16 13:51:31 -0500

    Failing test for one variable

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 
 assert.strictEqual(0,derivative("2"));
+assert.strictEqual(2,derivative("2x"));
 
+// assume variable is always named x
 function derivative(i) {
     return 0;
 }
